fe2fa272c50a27c394ae43b475f1001321b3fbc6
Tofaiel-1 <158508049+Tofaiel-1@users.noreply.github.com>
2025-06-03 10:32:33 +0600

    Add files via upload
    first  hello world run by c#

---
+++ C-Sharp Learn/hello-world.cs
@@ -0,0 +1,14 @@
+Result Size: 745 x 636
+using System;
+​
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
+Hello World!
