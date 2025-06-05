47bf402c4195ded9929b87b3123d0ab2e91f348b
Tofaiel-1 <158508049+Tofaiel-1@users.noreply.github.com>
2025-06-05 22:50:17 +0600

    Add files via upload
    constant variable that can't  change

---
+++ C-Sharp Learn/constant.cs
@@ -0,0 +1,13 @@
+using System;
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const int myNum = 15;
+      myNum = 20;
+      Console.WriteLine(myNum);
+    }
+  }
+}
